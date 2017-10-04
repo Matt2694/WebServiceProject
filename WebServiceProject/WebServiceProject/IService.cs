cd20adc384e1d2242fec0e61dc1fd0c8a0d809d5
Matthew Peterson <matt2694@edu.eal.dk>
2017-10-04 12:50:00 +0200

    added stufff so it still works

--- WebServiceProject/WebServiceProject/IService.cs
+++ WebServiceProject/WebServiceProject/IService.cs
@@ -11,6 +11,11 @@ namespace WebServiceProject
     [ServiceContract]
     public interface IService
     {
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
     }
 
     // Use a data contract as illustrated in the sample below to add composite types to service operations.
@@ -18,5 +23,21 @@ namespace WebServiceProject
     [DataContract]
     public class CompositeType
     {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
     }
 }
