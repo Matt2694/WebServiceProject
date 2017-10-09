97bd377db96321364da476f9335c8e50faf9e3f1
Matthew Peterson <matt2694@edu.eal.dk>
2017-10-09 11:10:30 +0200

    Testing Hosting the service

---
+++ WebServiceProject/WebServiceProjectHost/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebServiceProjectHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost webServiceProjectHost = null;
+            try
+            {
+                //Base Address for StudentService
+                Uri httpBaseAddress = new Uri("http://localhost:8733/Design_Time_Addresses/WebServiceProject/IzingaService/");
+
+                //Instantiate ServiceHost
+                webServiceProjectHost = new ServiceHost(typeof(WebServiceProjectTest.IzingaService),
+                    httpBaseAddress);
+
+                //Add Endpoint to Host
+                webServiceProjectHost.AddServiceEndpoint(typeof(WebServiceProjectTest.IIzingaService),
+                                                        new WSHttpBinding(), "");
+
+                //Metadata Exchange
+                ServiceMetadataBehavior serviceBehavior = new ServiceMetadataBehavior();
+                serviceBehavior.HttpGetEnabled = true;
+                webServiceProjectHost.Description.Behaviors.Add(serviceBehavior);
+
+                //Open
+                webServiceProjectHost.Open();
+                Console.WriteLine("Service is live now at: {0}", httpBaseAddress);
+                Console.ReadKey();
+            }
+
+            catch (Exception ex)
+            {
+                webServiceProjectHost = null;
+                Console.WriteLine("There is an issue with StudentService" + ex.Message);
+            }
+        }
+    }
+}
