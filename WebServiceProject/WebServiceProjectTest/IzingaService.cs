b6c5563c371f2dd11265465a6256cfb6dfdd8817
Matthew Peterson <matt2694@edu.eal.dk>
2017-10-11 12:01:41 +0200

    changed namespace name

--- WebServiceProject/WebServiceProjectTest/IzingaService.cs
+++ WebServiceProject/WebServiceProjectTest/IzingaService.cs
@@ -5,7 +5,7 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
 
-namespace WebServiceProjectTest
+namespace WebServiceProject
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
     public class IzingaService : IIzingaService
